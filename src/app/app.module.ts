4ec642378e5aa6744ebac32feb68b431e3812227
Mohd Haroon <haroonsaifi2001@gmail.com>
2023-10-06 22:17:37 +0530

    navbar and hero section completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { NavbarComponent } from './pages/home-page/navbar/navbar.component';
+import { HeroComponent } from './pages/home-page/hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavbarComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/home-page/navbar/navbar.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core'
+import { Router } from '@angular/router'
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+})
+export class NavbarComponent implements OnInit {
+  constructor(private router: Router) { }
+  ngOnInit(): void { }
+  public navStatus:boolean=false
+  toggleClass(list: HTMLDivElement): void {
+    if (list.style.right == '0px') {
+      list.style.right = '-100%'
+      this.navStatus=false
+    } else {
+      list.style.right = '0px'
+      this.navStatus=true
+    }
+  }
+  navigateToRoute() {
+    this.router.navigateByUrl('logincallback')
+  }
+}
